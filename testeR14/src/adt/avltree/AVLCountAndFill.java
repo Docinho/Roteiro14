726c6282e1c12441fcfbd0868c6bf7cc12255fe0
Lívia Cavalcanti <livia.juliao@ccc.ufcg.edu.br>
2017-07-26 14:49:35 -0300

    ainda errado

--- testeR14/src/adt/avltree/AVLCountAndFill.java
+++ testeR14/src/adt/avltree/AVLCountAndFill.java
@@ -4,14 +4,12 @@ import java.util.Arrays;
 
 import adt.bst.BSTNode;
 
-public class AVLCountAndFill <T extends Comparable<T>> extends AVLTreeImpl<T> {
-	
+public class AVLCountAndFill<T extends Comparable<T>> extends AVLTreeImpl<T> {
+
 	private int LLcounter;
 	private int LRcounter;
 	private int RRcounter;
 	private int RLcounter;
-	
-
 
 	public int LLcounter() {
 		return LLcounter;
@@ -28,7 +26,7 @@ public class AVLCountAndFill <T extends Comparable<T>> extends AVLTreeImpl<T> {
 	public int RLcounter() {
 		return RLcounter;
 	}
-	
+
 	protected void rebalance(BSTNode<T> node) {
 		if (node != null && !node.isEmpty()) {
 			int i = calculateBalance(node);
@@ -41,24 +39,44 @@ public class AVLCountAndFill <T extends Comparable<T>> extends AVLTreeImpl<T> {
 				}
 				leftRotation(node);
 			} else if (i > 1) {
-				if(calculateBalance((BSTNode<T>) node.getLeft()) < 0){
+				if (calculateBalance((BSTNode<T>) node.getLeft()) < 0) {
 					leftRotation((BSTNode<T>) node.getLeft());
 					LRcounter++;
 				} else
 					RRcounter++;
 				rightRotation(node);
-				
+
 			}
 		}
 	}
-	
+
 	public void fillWithoutRebalance(T[] array) {
 		Arrays.sort(array);
-		int index = array.length/2;
-				
-		for(int i = 0; i < array.length; i++) {
-			index = (int) (index + ((Math.pow(-1,i)*i)));
-			this.insert(array[index]);
+		int index = array.length / 2;
+		T root1 = null;
+		if (root.getData() != null && array[index].compareTo(root.getData()) > 0) {
+			root1 = root.getData();
+			root.setData(array[index]);
+			insert(root1);
+		} else
+			insert(array[index]);
+		for (int i = 0; i < array.length; i++) {
+			insere(array, 0, index);
+			insere(array, index + 1, array.length - 1);
 		}
+	}
+
+	private void insere(T[] array, int index0, int index1) {
+		if (index0 < index1) {
+			int mid = (int) Math.ceil((index1 + index0) / 2);
+			if (array[mid] != null) {
+				insert(array[mid]);
+				array[mid] = null;
+			} else {
+				insere(array, index0, mid);
+				insere(array, mid + 1, index1);
+			}
+		}
+
 	}
 }
